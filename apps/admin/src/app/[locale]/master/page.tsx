1bdb47629c7d05b209dd26c360abddbb245c6fbd
Saubh <saubhtech@gmail.com>
2026-02-21 01:53:40 +0530

    feat: add Markets to master data index page

--- apps/admin/src/app/[locale]/master/page.tsx
+++ apps/admin/src/app/[locale]/master/page.tsx
@@ -30,6 +30,7 @@ const TABLES = [
     items: [
       { key: 'sectors', label: 'Sectors', description: 'Industry sectors', icon: 'M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z' },
       { key: 'fields', label: 'Fields', description: 'Fields within sectors', icon: 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z' },
+      { key: 'markets', label: 'Markets', description: 'Products & services by field', icon: 'M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 100 4 2 2 0 000-4z' },
     ],
   },
 ];
